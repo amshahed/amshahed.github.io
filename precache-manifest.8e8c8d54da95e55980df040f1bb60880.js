9ef2c4ab10b811f314e30b1e2c45837e2a91bba3
amshahedhasan <amsh.himu@gmail.com>
2022-08-01 11:58:27 +0600

    Updates

---
+++ precache-manifest.8e8c8d54da95e55980df040f1bb60880.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb580947716e6006f8f7a5ec92d21b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3411fb4a0761f0dfeb7",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "a1a43e25ce28d66290c9",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "b3411fb4a0761f0dfeb7",
+    "url": "/static/js/2.6e20baee.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.6e20baee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a43e25ce28d66290c9",
+    "url": "/static/js/main.840e7aa2.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
